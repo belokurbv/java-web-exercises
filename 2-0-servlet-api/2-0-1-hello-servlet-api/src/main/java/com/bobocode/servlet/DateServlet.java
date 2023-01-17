f5e6a87239911563936b1536ba8bdec146e82559
Bohdan Belokur <belokur.bv@gmail.com>
2023-01-17 09:39:17 +0200

    HW 2

---
+++ 2-0-servlet-api/2-0-1-hello-servlet-api/src/main/java/com/bobocode/servlet/DateServlet.java
@@ -0,0 +1,23 @@
+package com.bobocode.servlet;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDate;
+
+@WebServlet("/date")
+public class DateServlet extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        PrintWriter out = response.getWriter();
+
+        out.println(LocalDate.now());
+       // out.println("</body></html>");
+    }
+}
